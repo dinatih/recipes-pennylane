bc52d8d677c257f53f894e1748a3dba0b3babfb3
dinatih <dinatih@gmail.com>
2021-03-14 21:16:39 +0100

    Update Recipe model with pg_search, validations and activestorage image

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class Recipe < ApplicationRecord
+  include PgSearch::Model
+
+  has_one_attached :image
+
+  validates :name, :ingredients, presence: true
+
+  pg_search_scope :search_by_ingredients,
+                  against: :ingredients,
+                  using: { tsearch: { highlight: { StartSel: '<b>', StopSel: '</b>', HighlightAll: true } } }
 end

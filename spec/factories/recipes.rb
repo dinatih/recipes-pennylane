6d26dd32e8c7bcbd543ec9bff8a38185275c97ea
dinatih <dinatih@gmail.com>
2021-03-14 21:42:49 +0100

    Add a spec for Recipe.search_by_ingredients(str)

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,17 +1,29 @@
 FactoryBot.define do
   factory :recipe do
-    name { "MyString" }
-    ingredients { "MyText" }
-    prep_time { "MyString" }
-    cook_time { "MyString" }
-    total_time { "MyString" }
-    people_quantity { 1 }
-    tags { "MyText" }
-    author { "MyString" }
-    budget { "MyString" }
-    difficulty { "MyString" }
-    rate { 1 }
-    author_tip { "MyText" }
-    nb_comments { 1 }
+    name { 'MyRecipe' }
+    ingredients { '[]' }
+
+    factory :recipe_with_sel_and_poivre1 do
+      name { 'Pates aux beurre' }
+      ingredients { '["pates", "beurre", "sel", "poivre"]' }
+    end
+    factory :recipe_with_sel_and_poivre2 do
+      name { 'Riz aux beurre' }
+      ingredients { '["riz", "beurre", "sel", "poivre"]' }
+    end
+
+    factory :recipe_with_sel_but_not_poivre do
+      name { 'Pates' }
+      ingredients { '["pates", "sel"]' }
+    end
+
+    factory :recipe_without_sel_and_poivre1 do
+      name { 'Salade de fruits' }
+      ingredients { '["pomme", "orange", "cerise"]' }
+    end
+    factory :recipe_without_sel_and_poivre2 do
+      name { 'Banana split' }
+      ingredients { '["Banane", "Chocolat"]' }
+    end
   end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.search_by_ingredients(str)' do
+    before do
+      FactoryBot.create :recipe_with_sel_and_poivre1
+      FactoryBot.create :recipe_with_sel_and_poivre2
+
+      FactoryBot.create :recipe_with_sel_but_not_poivre
+
+      FactoryBot.create :recipe_without_sel_and_poivre1
+      FactoryBot.create :recipe_without_sel_and_poivre2
+    end
+
+    it 'returns recipes with at least str\'s ingredients' do
+      recipes = described_class.search_by_ingredients('sel, poivre')
+
+      expect(recipes.count).to be(2)
+      recipes.each do |r|
+        expect(r.ingredients).to include('sel')
+        expect(r.ingredients).to include('poivre')
+      end
+    end
+  end
 end
